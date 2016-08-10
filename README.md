3f3eeaa6cb25f7443761a4ef8561cc70223827ae
Hong, Dewey <doohee323@gmail.com>
2016-08-10 11:17:43 +0900

    add s3 repository as uploads folder

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Run a Wordpress server on Vagrant
 
 install a wordpress server with ubuntu 14.04, MySQL, nginx, php 7.0. 
+make an aws s3 bucket as wordpress media repository.
+
+-. register AWS Access key
+```
+	cf. Your Security Credentials > Access Keys (Access Key ID and Secret Access Key)
+	change the key in /wordpress-vagrant/scripts/wordpress.sh 120 line
+	ex.  echo AKIAJOEN111111VX5SWQ:P/N6fMLdjxLjB11111111111111u1YsUJ7OxjkEB > /etc/passwd-s3fs
+```
 
 -. build a server
 ```
--- scripts/wordpress.sh
+++ scripts/wordpress.sh
@@ -20,6 +20,7 @@ sudo apt-get install nginx -y
 
 sudo cp $SRC_DIR/nginx/nginx.conf /etc/nginx/nginx.conf
 cp -Rf $SRC_DIR/nginx/default /etc/nginx/sites-enabled
+sudo service nginx stop
 sudo nginx -s stop
 sudo nginx
 # curl http://127.0.0.1:80
@@ -56,11 +57,17 @@ sudo /etc/init.d/mysql restart
 sudo apt-get install php7.0-fpm -y
 sudo apt-get install php7.0-mysql -y
 sudo sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/;error_log = php_errors.log/error_log = php_errors.log/g" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 200M/g" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/post_max_size = 8M/post_max_size = 200M/g" /etc/php/7.0/fpm/php.ini
+sudo sed -i "s/max_execution_time = 300/max_execution_time = 1000/g" /etc/php/7.0/fpm/php.ini
 
 sudo service php7.0-fpm restart
 
 ### [open firewalls] ############################################################################################################
 ufw allow "Nginx Full"
+sudo iptables -I INPUT -p tcp --dport 21 -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 3306 -j ACCEPT
@@ -84,10 +91,42 @@ sed -i "s/password_here/971097/g" $PROJ_DIR/wordpress/wp-config.php
 
 sudo rsync -avP $PROJ_DIR/wordpress/ /usr/share/nginx/html/
 
-cd /usr/share/nginx
-sudo chown -R www-data:www-data html/
 sudo usermod -a -G www-data www-data
+sudo chown -R www-data:www-data /usr/share/nginx/html/
 
 #curl http://192.168.82.170
+
+### [install ftp] ############################################################################################################
+apt-get install vsftpd
+sed -i "s/anonymous_enable=YES/anonymous_enable=NO/g" /etc/vsftpd.conf
+sed -i "s/local_enable=NO/local_enable=YES/g" /etc/vsftpd.conf
+sed -i "s/#write_enable=YES/write_enable=YES/g" /etc/vsftpd.conf
+sed -i "s/pam_service_name=vsftpd/pam_service_name=ftp/g" /etc/vsftpd.conf
+echo "www-data" >> /etc/ftpusers
+
+#useradd -g www-data -d /home/www-data -s /bin/bsh -m Hongdoohee!323
+service vsftpd restart
+
+### [install s3] ############################################################################################################
+cd
+sudo apt-get install automake autotools-dev g++ git libcurl4-gnutls-dev libfuse-dev libssl-dev libxml2-dev make pkg-config -y
+git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+cd s3fs-fuse
+./autogen.sh
+./configure
+make
+sudo make install
+
+echo AKIAJOEN111111VX5SWQ:P/N6fMLdjxLjB11111111111111u1YsUJ7OxjkEB > /etc/passwd-s3fs
+sudo chmod 600 /etc/passwd-s3fs
+
+mkdir -p /usr/share/nginx/html/wp-content/uploads
+sudo s3fs topzone /usr/share/nginx/html/wp-content/uploads -o nonempty -o allow_other 
+# sudo s3fs topzone /usr/share/nginx/html/wp-content/uploads -o passwd_file=/etc/passwd-s3fs -d -d -f -o f2 -o curldbg -o nonempty 
+# sudo umount /usr/share/nginx/html/wp-content/uploads
+# sudo fusermount -u /usr/share/nginx/html/wp-content/uploads
+# chown -Rf www-data:www-data /usr/share/nginx/html/wp-content/uploads
+# cf. s3fs topzone /usr/share/nginx/html/wp-content/uploads -o passwd_file=/etc/passwd-s3fs -d -d -f -o f2 -o curldbg
+echo "topzone /usr/share/nginx/html/wp-content/uploads fuse.s3fs _netdev,allow_other,dbglevel=dbg,curldbg 0 0" >> /etc/fstab
 
 exit 0
