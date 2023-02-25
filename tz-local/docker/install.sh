afca1cdd01b764ecd71d333b76100b9f7a46aaea
Dewey Hong <doohee.hong@ejn.kr>
2023-02-26 08:23:05 +0900

    Revert "Revert "done""
    This reverts commit dc73c615244121bd127dda22c19882e4853cd537.

--- tz-local/docker/install.sh
+++ tz-local/docker/install.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
 
-#cd /Volumes/workspace/tz/tz-wordpress/tz-local/tz-ubuntu
-#set -x
+git clone -b docker https://github.com/doohee323/tz-wordpress.git
+cd tz-wordpress
 
-docker container stop $(docker container ls -a -q) && docker system prune -a -f --volumes
+sudo wget http://wordpress.org/latest.tar.gz
+sudo tar xzvf latest.tar.gz
+
+sudo cp tz-local/docker/tz-wordpress/wp-config.php wordpress/wp-config.php
+
+cd tz-local/docker/
+#docker container stop $(docker container ls -a -q) && docker system prune -a -f --volumes
 #docker-compose -f docker-compose.yml build --no-cache
+#docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml build
 docker-compose -f docker-compose.yml up -d
-#docker-compose -f docker-compose.yml down
 #docker logs docker-nginx-1
+
+curl http://localhost:8080
 
 exit 0
 
+docker exec -it `docker ps | grep tz-wordpress | awk '{print $1}'` bash
+docker exec -it `docker ps | grep docker-nginx | awk '{print $1}'` bash
+
 DOCKER_ID=doohee323
-DOCKER_PASSWD=xxxxx
+DOCKER_PASSWD=xxxx
 APP=tz-wordpress
 BRANCH=latest
 docker login -u="${DOCKER_ID}" -p="${DOCKER_PASSWD}"
 TAG="${DOCKER_ID}/${APP}:${BRANCH}"
-echo "######### ${TAG} ##############"
 RMI=`docker images -a | grep ${DOCKER_ID}/${APP} | grep latest | awk '{print $3}'`
 echo docker tag ${RMI} ${TAG}
 docker tag ${RMI} ${TAG}
