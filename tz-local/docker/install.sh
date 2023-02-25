eef79e6989b834ca5f88a8a904d8e7386e8bcb9e
Dewey Hong <doohee.hong@ejn.kr>
2023-02-26 08:19:38 +0900

    test on windows

--- tz-local/docker/install.sh
+++ tz-local/docker/install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/doohee323/tz-wordpress.git
+git clone -b docker https://github.com/doohee323/tz-wordpress.git
 cd tz-wordpress
-git checkout -b docker origin/docker
 
 sudo wget http://wordpress.org/latest.tar.gz
 sudo tar xzvf latest.tar.gz
